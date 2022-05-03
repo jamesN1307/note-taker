f418a6397439b4f970dc29e3760b18bbd76e76a6
James Nguyen <nguyentri1307@gmail.com>
2022-05-03 01:04:25 -0700

    fixing bug

--- server.js
+++ server.js
@@ -1,75 +1,78 @@
-// require all necessary module
 const express = require('express');
-const { fsyncSync } = require('fs');
-const note_taker = express();
-const PORT = process.env.PORT || 3000;
-const fs = require('fs')
-let newInfo; // new data to be put into the existied file
-let notes; // existed file
-// route and middleware
-note_taker.use(express.json());
-note_taker.use(express.urlencoded({ extended: true }));
-note_taker.use(express.static('public'));
+const path = require('path');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid')
+const PORT = process.env.PORT || 3001;
+const takeNote = require('./Develop/db/db.json')
+const app = express();
 
-// function to update the page 
+let notes;
+
+//parsing data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//serves css/js/static assets
+app.use(express.static("public"))
+
+
+// get NOTE routes
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./Develop/public/index.html"))
+})
+app.get("/notes",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./Develop/public/notes.html"))
+})
+
+app.get("/db/db",(req,res)=>{
+    res.json(takeNote)
+})
+
 function reloadPage(){
-    fs.writeFile("db/db.json",JSON.stringify(notes),err => {
+    fs.write.File("db/db.json",JSON.stringify(notes), err => {
         if (err) throw err;
-        return true
+        return true;
     })
-
 }
-//starts the server
-note_taker.listen(PORT,() =>console.log(`NOTE_TAKER awaits your command on PORT ${PORT}`))
 
+// get method  
 
+app.get("/api/notes",(req,res)=>{
+    const note = JSON.parse(fs.readFileSync("./Develop/db/db.json","utf-8"))
+    res.json(note)
+})
+// post method 
+app.post("/api/notes",(req,res)=>{
+    const newNote = JSON.parse(fs.readFileSync("./Develop/db/db.json","utf-8"))
+    const { title, text} = req.body
+    if (req.body){
+        const urNote = {
+            title,
+            text,
+            id: uuidv4(),
+        }
+    newNote.push(urNote);
+    const readableNote = JSON.stringify(newNote,null,4);
+    fs.writeFileSync("./Develop/db/db.json/", readableNote)
+    res.json(`Your new note ${urNote.id} is now in a json file`)
+    }else{
+        throw err;
+    }
+})
 
-//routes
- //-> read file from db.json
- fs.readFile("db/db.json", "utf8", (err, data) => {
-    if (err) throw err
-    notes = JSON.parse(data);
- // ---------------------------------------------------------------------------------------
-    // GET method - 'get page', 'get listed item', 'get updated page'
-    note_taker.get("/notes", (ask, ans) => {
-        ans.sendFile(path.join(__dirname, "./public/notes.html"))
-    });
-
-    note_taker.get("/api/notes", (ask, ans) => {
-        ans.json(notes);
-    });
-
-    note_taker.get("/api/notes/:id", (ask, ans) => {
-        ans.JSON(notes[ask.params.id]);
-    });
-// ---------------------------------------------------------------------------------------
-
-    // GET method - HOME PAGE
-     note_taker.get('*', (ask, ans) => {
-        ans.sendFile(path.join(__dirname, "./public/index.html"));
-    });
-
-
- // ---------------------------------------------------------------------------------------
-
-    //POST method - 'to create new item and store it in data (JSON file)'
-    note_taker.post("/api/notes", (ask, ans) => {
-        newInfo = ask.body;
-        notes.push(newInfo);
-        reloadPage();
-        console.log('Database is updated: \n' + newInfo.title + ": " + newInfo.text);
-    });
-
- // ---------------------------------------------------------------------------------------
-
-    // Delete method - 'to remove the selected item from data, and update'
-    note_taker.delete("/api/notes/:id", (ask, ans) => {
-        notes.splice(ask.params.id, 1);
-        reloadPage();
-        console.log('Got a DELETE request!')
-    })
-
+app.delete("/api/notes",(req,res)=>{
+    const erase = JSON.parse(fs.readFileSync("./Develop/db/db.json"))
+    fs.writeFileSync("./db/db.json",JSON.stringify(erase.filter(filterNote => {
+        filterNote.id !== req.params.id
+    },null,4
+    )))
 })
 
 
-
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./Develop/public/index.html"))
+})
+app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`)
+})
